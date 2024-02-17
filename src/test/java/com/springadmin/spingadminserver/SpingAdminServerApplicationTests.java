013a03ac5d77df790777c3b92754fd80e11b5159
aprilnurf <aprilia.fauziyah@tiket.com>
2024-02-17 13:10:40 +0700

    init Springboot admin server

---
+++ src/test/java/com/springadmin/spingadminserver/SpingAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springadmin.spingadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
